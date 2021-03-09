046a6dd65e0d87e8231ec8334284899a44ccc084
Graham Eglit <grahammse@gmail.com>
2021-03-09 07:51:31 -0800

    add american manufacturer to plot (color and fill)

--- Untitled.R
+++ Untitled.R
@@ -7,8 +7,17 @@ skim(dat)
 vtable(dat)
 
 
-ggplot(dat, aes(mpg, wt)) + 
+ggplot(dat, aes(mpg, wt, color = as.factor(am), fill = as.factor(am))) + 
   geom_point() + 
   geom_smooth(method = "lm") + 
+  labs(
+    color = "American Manufacturer",
+    fill = "American Manufacturer",
+    y = "Weight",
+    x = "Miles Per Gallon"
+  ) + 
   facet_wrap(~gear)
+
+write_csv(dat, "mtcars.csv")
+
 
